5c10ef8963149194f652089bae857b403169c797
Stephen J. Carnam <steve@steveorevo.com>
2021-06-04 22:47:04 -0700

    init

---
+++ ds-phpmyadmin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: phpMyAdmin
+ * Plugin URL: https://serverpress.com/plugins/phpmyadmin
+ * Description: phpMyAdmin provided as a pre-configured DesktopServer plugin.
+ * Version: 5.1.1
+ * Author: Stephen J. Carnam
+ * Author URI: http://serverpress.com
+ *
+ */
---
+++ prepend.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Create a menu item within our localhost tools pull down menu.
+ */
+// global $ds_runtime;
+// if ( !$ds_runtime->is_localhost ) return; // Not localhost
+// if ( $ds_runtime->last_ui_event !== false ) return; // Not interested in events
+
+// /**
+//  * Add our menu to the localhost page.
+//  */
+// $ds_runtime->add_action( 'append_tools_menu', 'phpmyadmin_tools_menu' );
+// function phpmyadmin_tools_menu() {
+//   echo '<li><a href="http://localhost/ds-plugins/phpmyadmin/web/index.php">PHPMyAdmin - MySQL Administration</a></li>';
+// }
+
+// $ds_runtime->add_action( 'domain_button_group', 'phpmyadmin_domain_button_group' );
+// function phpmyadmin_domain_button_group($detail) {
+//   ?>
+//   <a href="http://localhost/ds-plugins/phpmyadmin/web/db_structure.php?db=<?php echo $detail->dbName; ?>" class="btn btn-warning" target="_blank">
+//       Database
+//   </a>
+//   <?php
+// }
