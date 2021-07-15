036a343f7bf510c05dc63216d02e77baed9bd0c7
Stephen J. Carnam <steve@steveorevo.com>
2021-07-14 18:59:53 -0700

    phpMyAdmin from tools menu

--- prepend.php
+++ prepend.php
@@ -2,23 +2,15 @@
 /**
  * Create a menu item within our localhost tools pull down menu.
  */
-// global $ds_runtime;
-// if ( !$ds_runtime->is_localhost ) return; // Not localhost
-// if ( $ds_runtime->last_ui_event !== false ) return; // Not interested in events
+global $ds_runtime;
+if ( !$ds_runtime->is_localhost ) return; // Not localhost
 
-// /**
-//  * Add our menu to the localhost page.
-//  */
-// $ds_runtime->add_action( 'append_tools_menu', 'phpmyadmin_tools_menu' );
-// function phpmyadmin_tools_menu() {
-//   echo '<li><a href="http://localhost/ds-plugins/phpmyadmin/web/index.php">PHPMyAdmin - MySQL Administration</a></li>';
-// }
-
-// $ds_runtime->add_action( 'domain_button_group', 'phpmyadmin_domain_button_group' );
-// function phpmyadmin_domain_button_group($detail) {
-//   ?>
-//   <a href="http://localhost/ds-plugins/phpmyadmin/web/db_structure.php?db=<?php echo $detail->dbName; ?>" class="btn btn-warning" target="_blank">
-//       Database
-//   </a>
-//   <?php
-// }
+/**
+ * Add our menu to the localhost page.
+ */
+$ds_runtime->add_action( 'append_tools_menu', 'phpmyadmin_tools_menu' );
+function phpmyadmin_tools_menu() {
+  echo '<b-dropdown-item href="http://localhost/ds-plugins/ds-phpmyadmin/app" target="_blank">
+            phpMyAdmin - MySQL Administration
+        </b-dropdown-item>';
+}
