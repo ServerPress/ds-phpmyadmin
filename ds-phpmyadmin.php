eec1c88acc1f382a782d1f33634325022111d5ef
Stephen J. Carnam <steve@steveorevo.com>
2021-08-11 17:10:46 -0700

    URI not URL according to spec

--- ds-phpmyadmin.php
+++ ds-phpmyadmin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: phpMyAdmin
- * Plugin URL: https://serverpress.com/plugins/phpmyadmin
+ * Plugin URI: https://serverpress.com/plugins/phpmyadmin
  * Description: phpMyAdmin provided as a pre-configured DesktopServer plugin.
  * Version: 5.1.1
  * Author: Stephen J. Carnam
